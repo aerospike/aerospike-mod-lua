dba428f9d217cee853959bf48bb95d3591408d49
Chris Stivers <chris@aerospike.com>
2012-11-28 19:25:00 -0800

    cleanup and added list handling

--- src/main/mod_lua_val.c
+++ src/main/mod_lua_val.c
@@ -4,6 +4,7 @@
 #include <lauxlib.h>
 #include <lualib.h>
 #include <string.h>
+#include <stdio.h>
 
 /**
  * Reads a val from the Lua stack
@@ -14,12 +15,15 @@
  */
 as_val * mod_lua_toval(lua_State * l, int i) {
     switch( lua_type(l, i) ) {
-        case LUA_TNUMBER :
+        case LUA_TNUMBER : {
             return (as_val *) as_integer_new((long) lua_tonumber(l, i));
-        case LUA_TBOOLEAN :
+        }
+        case LUA_TBOOLEAN : {
             return (as_val *) as_boolean_new(lua_toboolean(l, i));
-        case LUA_TSTRING :
+        }
+        case LUA_TSTRING : {
             return (as_val *) as_string_new(strdup(lua_tostring(l, i)));
+        }
         case LUA_TNIL :
         case LUA_TTABLE :
         case LUA_TFUNCTION :
@@ -36,19 +40,27 @@ as_val * mod_lua_toval(lua_State * l, int i) {
  *
  * @param l the lua_State to push the val onto
  * @param v the val to push on to the stack
- * @returns 0 if successful, otherwise 1
+ * @returns number of values pushed
  */
 int mod_lua_pushval(lua_State * l, const as_val * v) {
     switch( as_val_type(v) ) {
-        case AS_INTEGER:
-            lua_pushnumber(l, as_integer_toint(as_integer_fromval(v)) );
-            return 0;
-        case AS_STRING:
-            lua_pushstring(l, as_string_tostring(as_string_fromval(v)) );
-            return 0;
-        default:
+        case AS_INTEGER: {
+            lua_pushinteger(l, as_integer_toint((as_integer *) v) );
+            return 1;
+        }
+        case AS_STRING: {
+            lua_pushstring(l, as_string_tostring((as_string *) v) );
+            return 1;   
+        }
+        case AS_LIST: {
+            printf("mod_lua_pushval: AS_LIST\n");
+            mod_lua_pushlist(l, (as_list *) v);
+            return 1;   
+        }
+        default: {
             lua_pushnil(l);
-            return 0;
+            return 1;
+        }
     }
     return 0;
 }
