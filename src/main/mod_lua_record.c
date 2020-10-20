57ee412f1089a9c2557b4e566b9dbf33fdbbfbbe
Andrew Gooding <andy@aerospike.com>
2020-10-20 15:26:54 -0700

    AER-6317 - added record.memory_size() UDF method.

--- src/main/mod_lua_record.c
+++ src/main/mod_lua_record.c
@@ -101,6 +101,16 @@ static int mod_lua_record_gen(lua_State * l) {
 }
 
 /**
+ * Get a record's memory storage size:
+ *      record.memory_size(r)
+ */
+static int mod_lua_record_memory_size(lua_State * l) {
+    as_rec * rec = (as_rec *) mod_lua_checkrecord(l, 1);
+    lua_pushinteger(l, as_rec_memory_size(rec));
+    return 1;
+}
+
+/**
  * Get a record's device storage size:
  *      record.device_size(r)
  */
@@ -277,6 +287,7 @@ static const luaL_reg object_table[] = {
     {"ttl",        mod_lua_record_ttl},
     {"last_update_time", mod_lua_record_last_update_time},
     {"gen",        mod_lua_record_gen},
+    {"memory_size", mod_lua_record_memory_size},
     {"device_size", mod_lua_record_device_size},
     {"key",        mod_lua_record_key},
     {"setname",    mod_lua_record_setname},
